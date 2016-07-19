7b425903b8d0c21a48d634ab609ab2c56355f295
飯嶋洋行 <iijimahiroyuki@iijimahiroyuki-no-MacBook-Pro.local>
2016-07-19 09:17:18 +0900

    delete unnecessary directory

--- app/store/configureStore.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from 'redux';
-import reducer from '../reducers/index';
-
-export default function configureStore() {
-    const store = createStore(reducer);
-    return store;
-}
